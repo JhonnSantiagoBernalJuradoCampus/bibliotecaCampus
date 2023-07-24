88f31d802f7f8bd42ccde16a6c3011c2cda4ea12
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-24 07:21:08 -0500

    feat: :sparkles: Get todas las categorias
    Ruta, documentacion

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ MY_CONFIG = {"hostname": "127.12.1.2", "port": 5010}
 2. Abrir la terminal del editor de codigo y ejecutar el comando `npm i`
 # Endpoints
 1. Obtener los autores y su nacionalidad: `http://127.12.1.2:5010/autor`
+2. Listar todas las categorias disponibles: `http://127.12.1.2:5010/categoria`
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import appAutor from "./routers/autor.js";
+import appCategoria from "./routers/categoria.js";
 dotenv.config();
 let appExpress = express();
 
 appExpress.use(express.json());
-appExpress.use('/autor', appAutor)
+appExpress.use('/autor', appAutor);
+appExpress.use('/categoria', appCategoria);
 
 let myConfig = JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(myConfig, ()=>{
